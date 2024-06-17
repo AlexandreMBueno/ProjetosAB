8e89d1792bbaa3b49c5123e773687a961050f645
Alexandre <alexandre.bueno0204@gmail.com>
2024-06-17 11:57:35 -0300

    fix binary tree

--- NeetCode150/InvertBinaryTree.py
+++ NeetCode150/InvertBinaryTree.py
@@ -32,3 +32,8 @@ def print_tree(node):
     return [node.val] + print_tree(node.left) + print_tree(node.right) if node else []
 
 print(print_tree(inverted_root))  # output deve ser: [1, 3, 7, 6, 2, 5, 4]
+
+# usamos:
+# DFS(Depth-First Search)é usado para buscar elementos em estruturas de dados como arvores e grafos.
+# O algoritmo começa em um no raiz
+# e explora o max possivel ao longo de cada ramo antes de retroceder.
