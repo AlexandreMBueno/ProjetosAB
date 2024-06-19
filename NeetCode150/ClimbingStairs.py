4555a8ca745cfa6d04cfa70fda4ea4a946f02bf1
Alexandre <alexandre.bueno0204@gmail.com>
2024-06-19 13:58:23 -0300

    add comments on climbing stairs exercise -> fibonacci

--- NeetCode150/ClimbingStairs.py
+++ NeetCode150/ClimbingStairs.py
@@ -14,3 +14,8 @@ print(f"Input: n = {n}\nOutput: {result}")
 # 1 1 1
 # 1 2
 # 2 1
+
+# o número de maneiras de chegar ao degrau n é:
+# a soma do número de maneiras de chegar ao degrau n-1 e ao degrau n-2.
+
+# f(n) = f(n - 1) + f(n - 2) FIBONACCI
